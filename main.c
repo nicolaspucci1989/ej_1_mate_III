a756567749be26ec1784ab1caf16bcf6c3ef08d4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2016-12-14 20:56:13 -0300

    cambios menores

--- main.c
+++ main.c
@@ -19,11 +19,13 @@ int main()
 
   while((opcion = menu()) != SALIDA) {
     switch (opcion) {
+
       case 1:
         /*1.	Provincia que recibio mas presentaciones en general*/
         printf("Provincia que recibio mas prestaciones %s\n",
          provincias[provinciaQueRecibioMasPrestaciones(fact)]);
         break;
+
       case 2:
         /*2.	Monto total descontado por cada provincia sobre
         cada tipo de servicio*/
@@ -31,10 +33,12 @@ int main()
         totalDeDescuentoPorProvincia(fact, descuentos, totalDescuentos);
         imprimirDescuentos(totalDescuentos);
         break;
+
       case 3:
         /*3.	Para cada provincia, que servicio fue el menos utilizado*/
         servicioMenosUtilizadoPorProvincia(fact, provincias);
         break;
+
       case 4:
         /*4.	Ordene de menor a mayor el total descontado por cada tipo de
         servicio en general*/
@@ -42,8 +46,10 @@ int main()
         totalDeDescuentoPorProvincia(fact, descuentos, totalDescuentos);
         totalDescontadoPorServicio(totalDescuentos);
         break;
+
       case 5:
         ingresoFacturas(fact, numeroDeRegistro);
+        
       default:
         puts("Opcion incorrecta");
         break;
